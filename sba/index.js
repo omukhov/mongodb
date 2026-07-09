f300206741a19de30bdc48a6e7fa936d99ef747a
Artem Omukhov <omukhov@Artems-Air.lan>
2026-07-09 15:23:53 -0400

    Comment: Added comment for index.js

--- sba/index.js
+++ sba/index.js
@@ -5,26 +5,30 @@ import movies from "./routes/movies.js";
 import comments from "./routes/comments.js";
 import users from "./routes/users.js";
 
+// Dotenv config
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(express.json()); //register express built in json
+// Register express built in json
+app.use(express.json());
 
+// Lets you reaad from data in req.body
 app.use(
   express.urlencoded({
-    // lets you reaad from data in req.body
     extended: true,
   }),
 );
 
 app.use("/static", express.static("public"));
 
+// Routes
 app.use("/movies", movies);
 app.use("/comments", comments);
 app.use("/users", users);
 
+// Main
 app.get("/", (req, res) => {
   res.json("main");
 });
